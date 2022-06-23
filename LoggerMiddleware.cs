63447c827b5a68696b9dcabe29ac219d066882c2
RisanM <risanm-i@zillione.com>
2022-06-24 04:25:16 +0530

    middleware custom

---
+++ LoggerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace middlewareStuff
+{
+    public class LoggerMiddleware : IMiddleware
+    {
+        //To use this register this to dotnet core service colletion
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            context.Response.WriteAsync("Hello world \n");
+
+            await next(context);
+            
+            context.Response.WriteAsync("After Request");
+
+            // next is the next middleware
+            // so app.use is used to chain the middlewares
+
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,28 +12,60 @@ namespace middlewareStuff
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //Register the Custom middlewares
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<LoggerMiddleware>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        // Middleware are used handle request and responses
+        // Middlewares are configured inside the Configure method
+        // When there are no middlewares it will return a 404
+
+        
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // simple method method handle requests and return hello world
+            // This will handle any http context and return helloworld
 
-            app.UseRouting();
+            // So now this is called a Request delegate or .net Middleware
 
-            app.UseEndpoints(endpoints =>
+            //app.Run(async context => await context.Response.WriteAsync("Hello world")); //This is an inline middleware
+
+            //On app builder class we can use 3 methods to configure request delegates
+
+            // Run, Use, and Map
+
+            // Run - Terminal delegate - used to end the pipeline
+            // Use - Chain the middlewares
+            // Map - Branch the pipeline
+
+            //app.Run(async context => await context.Response.WriteAsync("hello again")); //This won't run
+
+            //app.Use(async (context, next) =>
+            //{
+            //    context.Response.WriteAsync("Hello world \n");
+            //    await next();
+            //    context.Response.WriteAsync("After Request");
+            //});
+
+            app.UseMiddleware<LoggerMiddleware>(); 
+
+            app.Map("/map", MapHandler);
+
+            app.Run(async context => await context.Response.WriteAsync("hello again \n"));
+
+
+        }
+
+        private void MapHandler(IApplicationBuilder app)
+        {
+            app.Run(async context =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Helllo from map \n");
             });
         }
     }
